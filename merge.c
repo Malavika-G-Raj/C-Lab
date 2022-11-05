2122d5a18e8bf5ae86eb68a1429a828e9027170c
Malavika-G-Raj <95291694+Malavika-G-Raj@users.noreply.github.com>
2022-11-05 11:23:19 +0530

    Update merge.c

--- merge.c
+++ merge.c
@@ -4,13 +4,14 @@ void main()
  int a[100],b[100],c[100],n,m,i,j;
  printf("Enter size of array\n");
  scanf("%d %d",&n,&m);
+ printf("\n Enter first array elemnts\n");
  for(i=0;i<m;i++)
 {
  scanf("%d",&a[i]);
  c[i]=a[i];
 }
  j=i;
- printf("\n Enter array elemnts\n");
+ printf("\n Enter 2 nd array elemnts\n");
  for(i=0;i<n;i++)
 {
  scanf("%d",&b[i]);
@@ -20,6 +21,6 @@ void main()
  printf("\n The new array\n");
  for(i=0;i<j;i++)
 {
- printf("%d",c[i]);
+ printf("%d \t",c[i]);
 }
 }
